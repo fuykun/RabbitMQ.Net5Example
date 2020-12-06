0e931bf2a13e571835269dd28f4a75a8eeebaf9c
Furkan Uykun <frkan.uykun@hotmail.com>
2020-12-06 20:10:38 +0300

    Update

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -4,15 +4,18 @@ using RabbitMQ.Client.Events;
 using System;
 using System.Text;
 
+const string hostName = "localhost";
+const string queueName = "mail";
 
 // RabbitMQ bağlantısı oluşturuyoruz.
-var factory = new ConnectionFactory() { HostName = "localhost" };
+var factory = new ConnectionFactory() { HostName = hostName };
 using (var connection = factory.CreateConnection())
 using (var channel = connection.CreateModel())
 {
+    Console.WriteLine($"'{hostName}' üzerinde ki RabbitMQ server'a bağlanıldı.");
 
     // 'mail' kuyruğuna erişiyoruz.
-    channel.QueueDeclare(queue: "mail", durable: false, exclusive: false, autoDelete: false, arguments: null);
+    channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
 
     // 'mail' kuyruğundaki verileri alıyoruz.
     var consumer = new EventingBasicConsumer(channel);
@@ -23,9 +26,9 @@ using (var channel = connection.CreateModel())
         var email = JsonConvert.DeserializeObject<Mail>(message);
         Console.WriteLine($"\nTarih: {email.mailTime}\nBaşlık: {email.Title}\nMail Adresi: {email.MailAddress}\nMesaj: {email.Content}");
     };
-    channel.BasicConsume(queue: "mail", autoAck: true, consumer: consumer);
+    channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
 
-    Console.WriteLine(" Mesaj başarılı ile kuyrukdan alındı.");
+    Console.WriteLine("Kuyruktaki mesajlar başarılı ile alındı.");
 }
 Console.ReadLine();
 
--- Producer/Program.cs
+++ Producer/Program.cs
@@ -3,14 +3,18 @@ using System.Text;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
 
+const string hostName = "localhost";
+const string queueName = "mail";
 
 // RabbitMQ bağlantısı oluşturuyoruz.
-var factory = new ConnectionFactory() { HostName = "localhost" };
+var factory = new ConnectionFactory() { HostName = hostName };
 using (var connection = factory.CreateConnection())
 using (var channel = connection.CreateModel())
 {
+    Console.WriteLine($"'{hostName}' üzerinde ki RabbitMQ server'a bağlanıldı.");
+
     // 'mail' isimli kuyrukla işlem yapacağımızı belirtiyoruz.
-    channel.QueueDeclare(queue: "mail", durable: false, exclusive: false, autoDelete: false, arguments: null);
+    channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
 
     // göndereceğimiz mail nesnesini tanımlıyoruz ve json'a çeviriyoruz.
     var email = new Mail(DateTime.Now, "mailaddress@mail.com", "Title", "Content");
@@ -19,8 +23,10 @@ using (var channel = connection.CreateModel())
     // kuyruğa gönderilecek mesajı byte'a çevirmemiz gerekiyor.
     var body = Encoding.UTF8.GetBytes(message);
 
+    Console.WriteLine($"'{queueName}' adlı kuyruğa mesaj gönderiliyor...");
+
     // mesajı RabbitMQ'dya ekliyoruz
-    channel.BasicPublish(exchange: "", routingKey: "mail", basicProperties: null, body: body);
+    channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: null, body: body);
 
     Console.WriteLine("Mail başarıyla kuyruğa eklendi.");
 }
